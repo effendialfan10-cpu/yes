bbf1efee7c1eb7d17eba864a49df3cb827c5f543
effendialfan10-cpu <effendialfan10@gmail.com>
2025-10-23 10:11:47 +0700

    ui

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tolmoblsapa = document.getElementByid('sapabutton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+Tombolsapa.addEventlistener('click', funtion() {
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Halo! TErimakasih sudah berkunjung!');
+});
